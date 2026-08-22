d198ea0a9d8b9d756b1d3e199b8406944558cde5
Konstantin Aksenov <kav.pbg@gmail.com>
2026-08-22 11:15:15 -0300

    chore: add pnpm/ESLint/Prettier/Renovate tooling, CI workflow and lint/build fixes

---
+++ src/app/main-content/find-us/find-us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './find-us.component.html',
+  styleUrl: './find-us.component.scss',
+})
+export class FindUsComponent {}
